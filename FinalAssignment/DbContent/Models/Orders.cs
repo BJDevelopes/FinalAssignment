58db0ced9208589493ca50193d29acd08ce0bb33
BJDevelopes <74470238+BJDevelopes@users.noreply.github.com>
2022-08-12 19:27:48 -0400

    Updates to _Layout & Added Orders Model

---
+++ FinalAssignment/DbContent/Models/Orders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalAssignment.Models
+{
+    [Table("Orders")]
+    public class Orders
+    {
+        public int Id { get; set; }      
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public string postal_code { get; set; }
+        public string products { get; set; }
+        public string hasshipped { get; set; }
+    }
+
+}
