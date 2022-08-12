9e5fd15822be88291af66c62debfb5fcb41fb728
BJDevelopes <74470238+BJDevelopes@users.noreply.github.com>
2022-08-12 19:53:02 -0400

    Updated Orders Model File Formatting + Web.config

---
+++ FinalAssignment/DbContent/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace FinalAssignment.Models
+{
+    [TableName("Orders")]
+    public class Orders
+    {
+        public int id { get; set; }
+        public string userID { get; set; }
+        public string productID { get; set; }
+        public int total { get; set; }
+    }
+}
